9343abe48de01390fb249b883bc40db708ab04b7
ConveningMoon <dj.vergara@hotmail.com>
2024-09-05 14:23:15 +0500

    adding vercel json settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import TestConnection from './components/TestConnection';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import TestConnection from './components/TestConnection';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/test" element={<TestConnection />} />
+        <Route path="/test" element={<TestConnection />} /> */}
       </Routes>
     </Router>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import ContentHome from '../components/ContentHome';
 import styles from './styles/Home.module.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Home: React.FC = () => {
   return (
     <div className={styles.homeStyle}>
       <Navbar />
       <ContentHome />
+      <SpeedInsights />
     </div>
   );
 };
