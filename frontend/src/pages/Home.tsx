a08b04a02d3542eba2a31f83e8bffd52a8c5a901
ConveningMoon <dj.vergara@hotmail.com>
2024-09-05 17:08:11 +0500

    removing suspense and lazy for home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import ContentHome from '../components/ContentHome';
 import styles from './styles/Home.module.css';
