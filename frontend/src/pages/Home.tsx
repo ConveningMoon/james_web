1cc070658e5004d37a9b48506c9521babae41b80
ConveningMoon <dj.vergara@hotmail.com>
2024-09-05 17:06:10 +0500

    removing suspense and lzy for home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,22 @@
 import React, { Suspense } from 'react';
-// import Navbar from '../components/Navbar';
-// import ContentHome from '../components/ContentHome';
+import Navbar from '../components/Navbar';
+import ContentHome from '../components/ContentHome';
 import styles from './styles/Home.module.css';
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Lazy load the ContentHome component
-const LazyContentHome = React.lazy(() => import('../components/ContentHome'));
-const LazyNavbar = React.lazy(() => import('../components/Navbar'));
+// const LazyContentHome = React.lazy(() => import('../components/ContentHome'));
+// const LazyNavbar = React.lazy(() => import('../components/Navbar'));
 
 const Home: React.FC = () => {
   return (
     <div className={styles.homeStyle}>
-      <Suspense fallback={<div>Loading content...</div>}>
-        <LazyNavbar />
-        <LazyContentHome />
-      </Suspense>
+      <Navbar />
+      <ContentHome />
+      {/* <Suspense fallback={<div>Loading content...</div>}> */}
+        {/* <LazyNavbar />
+        <LazyContentHome /> */}
+      {/* </Suspense> */}
       <SpeedInsights />
     </div>
   );
